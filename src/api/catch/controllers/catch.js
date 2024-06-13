a1dd84ab0466950ce6129eb21d08905c72fdac50
Ray Keating <raykeating13@gmail.com>
2024-06-12 23:10:49 -0400

    add angler to catch in create controller

--- src/api/catch/controllers/catch.js
+++ src/api/catch/controllers/catch.js
@@ -1,9 +1,36 @@
-'use strict';
+"use strict";
 
 /**
  * catch controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::catch.catch');
+module.exports = createCoreController("api::catch.catch", {
+  async create(ctx) {
+    // check if the user is logged in
+    if (!ctx.state.user) {
+      return ctx.unauthorized("You must be logged in to post a catch");
+    }
+
+    // add the current user to the request body
+    const result = await super.create(ctx);
+
+    // get the current user
+    const user = ctx.state.user;
+
+    // get the angler
+    const angler = await strapi.query("api::angler.angler").findOne({
+      user: user.id,
+    });
+
+    // add the user to the angler
+    await strapi.entityService.update("api::catch.catch", result.data.id, {
+      data: {
+        angler: angler.id
+      },
+    });
+
+    return result;
+  },
+});
